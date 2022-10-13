9e6d7069d5f0085a806fbf8243ac252cf433e839
matthew <permahuman@gmail.com>
2022-10-13 11:14:09 -0700

    Add js code to clear numbers display and two event listeners one for clearing display and one for adding zeros to the display.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+let display = document.querySelector('.numbersDisplay')
 
+let clearDisplay = document.querySelector(".clearDisplay")
+
+
+
+let withoutCommas;
+
+let displayVar = []
+
+let num0 = document.querySelector('.num0')
+
+num0.addEventListener('click', function(item) {
+
+displayVar.push(0)
+displayVar.join('')
+newValue = displayVar.toString()
+withoutCommas = newValue.replace(/,/g, '');
+if(withoutCommas.length >= 15){return}
+
+  else  {display.textContent = withoutCommas}
+
+})
+
+clearDisplay.addEventListener('click', function(e)
+{
+withoutCommas = ''
+display.textContent = ''
+})
