8a970be3f750472e625738729679149a479bf889
matthew <permahuman@gmail.com>
2022-10-13 11:25:44 -0700

    Add numbers display styling to make the display stay the same size whether their is text in it or not.

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ let displayVar = []
 
 let num0 = document.querySelector('.num0')
 
-num0.addEventListener('click', function(item) {
+num0.addEventListener('click', function(item) { //event listener to add zeros to numbers display
 
 displayVar.push(0)
 displayVar.join('')
 newValue = displayVar.toString()
 withoutCommas = newValue.replace(/,/g, '');
 
-if (withoutCommas.length >= 15) {return}
+if (withoutCommas.length >= 9) {return}
 display.textContent = withoutCommas
 })
 
-clearDisplay.addEventListener('click', function(e)
+clearDisplay.addEventListener('click', function(e)  //event listener to clear the numbers display
 {
 withoutCommas = ''
 display.textContent = ''
