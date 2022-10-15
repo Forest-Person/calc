46a8bcfd010335b7a60ba0f205b300804baae849
matthew <permahuman@gmail.com>
2022-10-15 10:04:22 -0700

    Update numberbox symbols background colors and operands positioning in their own grid.

--- index.js
+++ index.js
@@ -5,19 +5,39 @@ let clearDisplay = document.querySelector(".clearDisplay")
 let symbolButton = document.querySelectorAll('.symbols')
 
 symbolButton.forEach((item)=>
-item.addEventListener('click', function(e) { //event listener to add zeros to numbers display
+item.addEventListener('click', function(e) { //event listener to add numbers to numbers display
 
 
-return display.value += e.target.textContent
+ display.value += e.target.textContent
 
 
 }))
+
+
 
 clearDisplay.addEventListener('click', function(e)  //event listener to clear the numbers display
 {
 
 display.value = ''
 
+})
+
+
+
+let plusSign = document.querySelector('.plusSign')
+
+let var1 = 0;
+
+let var2 = 0;
+
+plusSign.addEventListener('click', function(){
+let displayVariable = display.value
+
+displayVariable + display.value
+
+display.value = displayVariable + display.value
+
+///trying to create a funciton with the plus sign to add two numbers together
 })
 
 
