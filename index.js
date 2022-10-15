d5b39e8747fc2b0f96870096542da97285ba7f03
matthew <permahuman@gmail.com>
2022-10-15 14:41:26 -0700

    Create event listeners for minus divides and mulitply signs. Can now do basic operations on them.

--- index.js
+++ index.js
@@ -19,12 +19,15 @@ clearDisplay.addEventListener('click', function(e)  //event listener to clear th
 {
 
 display.value = ''     //event listener to clear the display value and the numbers display
-
+plusSign.style.cssText = "background-color:aqua-marine;"
+minusSign.style.cssText = "background-color:aqua-marine;" ///resets the colors of the buttons of cleardisplay utton is pushed
+divideSign.style.cssText = "background-color:aqua-marine;"
+multiplySign.style.cssText = "background-color:aqua-marine;"
 })
 
 
 
-let plusSign = document.querySelector('.plusSign')
+
 
 let operatorBeingUsed = ''
 
@@ -35,14 +38,49 @@ let var2 = 0;
 let totalValue = 0;
 
 
-plusSign.addEventListener('click', function(){
 
+let plusSign = document.querySelector('.plusSign')
+plusSign.addEventListener('click', function(){  //Eventlister which updates var1 value and remove display.value
+                                                //creates string flag and makes the operator button red to signal usage
 var1 = display.value
 display.value = ''
 operatorBeingUsed = plusSign.textContent
 plusSign.style.cssText = "background-color:red;"
 
-///trying to create a funciton with the plus sign to add two numbers together
+
+})
+
+let minusSign = document.querySelector('.minusSign')
+minusSign.addEventListener('click', function(){  //Eventlister which updates var1 value and remove display.value
+    //creates string flag and makes the operator button red to signal usage
+var1 = display.value
+display.value = ''
+operatorBeingUsed = minusSign.textContent
+minusSign.style.cssText = "background-color:red;"
+
+
+})
+
+let divideSign = document.querySelector('.divideSign')
+divideSign.addEventListener('click', function(){  //Eventlister which updates var1 value and remove display.value
+    //creates string flag and makes the operator button red to signal usage
+var1 = display.value
+display.value = ''
+operatorBeingUsed = divideSign.textContent
+divideSign.style.cssText = "background-color:red;"
+
+
+})
+
+let multiplySign = document.querySelector('.multiplySign')
+multiplySign.addEventListener('click', function(){  //Eventlister which updates var1 value and remove display.value
+    //creates string flag and makes the operator button red to signal usage
+var1 = display.value
+display.value = ''
+operatorBeingUsed = multiplySign.textContent
+multiplySign.style.cssText = "background-color:red;"
+
+
 })
 
 
@@ -50,13 +88,28 @@ plusSign.style.cssText = "background-color:red;"
 equalSign = document.querySelector('.equalSign')
 
 equalSign.addEventListener('click', function(item) {
-     console.log(operatorBeingUsed)
+     
     
     var2 = display.value
 
     if(operatorBeingUsed == '+') 
     
     {totalValue = Number(var1) + Number(var2)}
+
+    if(operatorBeingUsed == '-')
+
+    {totalValue = Number(var1) - Number(var2)}
+
+    if(operatorBeingUsed == '/')
+
+    {totalValue = Number(var1) / Number(var2)}
+
+    if(operatorBeingUsed == 'x')
+
+    {totalValue = Number(var1) * Number(var2)}
+
+    if(operatorBeingUsed == '')
+    {display.value = ''}
 
      
      display.value = totalValue
@@ -66,5 +119,10 @@ equalSign.addEventListener('click', function(item) {
      var2 = ''
     
      plusSign.style.cssText = "background-color:aqua-marine;"
+     minusSign.style.cssText = "background-color:aqua-marine;"
+     divideSign.style.cssText = "background-color:aqua-marine;"
+     multiplySign.style.cssText = "background-color:aqua-marine;"
+
+     
 
 })
